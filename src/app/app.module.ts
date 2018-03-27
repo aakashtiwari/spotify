d5e191226fff63c755eb755aa7fa7a60b7a0750a
Aakash Tiwari <akashtiwari82@gmail.com>
2018-03-27 16:35:27 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MusicModule } from './music/music.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+        { path: 'home', component: HomeComponent },
+        { path: '', redirectTo: 'home', pathMatch: 'full'},
+        { path: '**', redirectTo: 'home', pathMatch: 'full'}
+    ]),
+    MusicModule
   ],
   providers: [],
   bootstrap: [AppComponent]
